6e7830da9b91e4194b86da7be0f6950131d779ea
suhaila hawsawi <suhila0987@outlook.com>
2025-11-17 14:23:51 +0300

    add 2nd test case

--- src/test/java/tests/US04_Profile.java
+++ src/test/java/tests/US04_Profile.java
@@ -1,5 +1,6 @@
 package tests;
 
+import base_urls.BaseApi;
 import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -9,6 +10,11 @@ public class US04_Profile {
 
     @Test(description = "[US04_TC001] Retrieve current user profile with valid token")
     public void testRetrieveCurrentUserProfile() {
+        // If no token exists, login first
+        if (BaseApi.getToken() == null) {
+            String token = BaseApi.loginAndGetToken("sara@example.com", "Pass123!");
+            BaseApi.setToken(token);
+        }
 
         // Call GET /profile using ApiUtil (BaseApi adds token automatically)
         Response response = ApiUtil.get("/me");
@@ -20,6 +26,19 @@ public class US04_Profile {
         Assert.assertNotNull(response.jsonPath().getString("id"), "User ID should be present");
         Assert.assertNotNull(response.jsonPath().getString("name"), "User name should be present");
         Assert.assertNotNull(response.jsonPath().getString("email"), "User email should be present");
+
+        System.out.println("Profile Response: " + response.asString());
+    }
+
+    @Test(description = "[US04_TC002] Retrieve current user profile with invalid token")
+    public void testRetrieveCurrentUserProfilewitoutToken() {
+
+        // Call GET /profile using ApiUtil (BaseApi adds token automatically)
+        Response response = ApiUtil.get("/me");
+
+        // Validate status
+        Assert.assertNull(BaseApi.getToken());
+        Assert.assertEquals(response.statusCode(), 401, "Expected status 401 for profile");
 
         System.out.println("Profile Response: " + response.asString());
     }
